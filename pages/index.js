0d35e2dcce288a89bd53a1e4e220d607fe30825d
Clinton Gallivan <clintongallivan@Clintons-MacBook-Pro.local>
2022-04-26 13:35:02 -0700

    updated class to classname

--- pages/index.js
+++ pages/index.js
@@ -6,124 +6,124 @@ export default function Home() {
   return (
     <>
       <body>
-        <div class="container">
-          <div class="row">
-            <div class="col-sm-3">&nbsp;</div>
-            <div class="col-sm-6">
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-3">&nbsp;</div>
+            <div className="col-sm-6">
               <center>
                 {' '}
                 <img
                   src="logo.png"
                   width="50%"
                   height="50%"
-                  class="img-responsive"
+                  className="img-responsive"
                   alt="Makmal Kesihatan Awam Kebangsaan"
                 />
                 https://simka-outbreak.moh.gov.my
               </center>
             </div>
-            <div class="col-sm-3">&nbsp;</div>
+            <div className="col-sm-3">&nbsp;</div>
           </div>
 
-          <form class="form-horizontal">
-            <div class="row">
-              <div class="col-sm-3">&nbsp;</div>
-              <div class="col-sm-6">
-                <div class="form-group">
-                  <label class="control-label col-sm-4" for="param2">
+          <form className="form-horizontal">
+            <div className="row">
+              <div className="col-sm-3">&nbsp;</div>
+              <div className="col-sm-6">
+                <div className="form-group">
+                  <label className="control-label col-sm-4" for="param2">
                     Nama Pesakit / <i>Patient Name</i> :{' '}
                   </label>
-                  <div class="col-sm-8">
+                  <div className="col-sm-8">
                     <input
                       style={{ backgroundColor: '#eee' }}
                       type="email"
-                      class="form-control"
+                      className="form-control"
                       value="SHANNA GALLIVAN"
                       readonly
                     />
                   </div>
                 </div>
-                <div class="form-group">
-                  <label class="control-label col-sm-4" for="param3">
+                <div className="form-group">
+                  <label className="control-label col-sm-4" for="param3">
                     ID Pemohon / <i>Patient ID</i> :{' '}
                   </label>
-                  <div class="col-sm-8">
+                  <div className="col-sm-8">
                     <input
                       style={{ backgroundColor: '#eee' }}
                       type="text"
-                      class="form-control"
+                      className="form-control"
                       value="530569 ******* (6 digit pertama / first 6 digits)"
                       readonly
                     />
                   </div>
                 </div>
-                <div class="form-group">
-                  <label class="control-label col-sm-4" for="param3">
+                <div className="form-group">
+                  <label className="control-label col-sm-4" for="param3">
                     Makmal Rujukan / <i>Reference Laboratory</i> :{' '}
                   </label>
-                  <div class="col-sm-8">
+                  <div className="col-sm-8">
                     <textarea
                       style={{ backgroundColor: '#eee' }}
                       rows="2"
-                      class="form-control"
+                      className="form-control"
                       readonly
                     >
                       KLINIK PRO MEDIC (KOTA KEMUNING)
                     </textarea>
                   </div>
                 </div>
-                <div class="form-group">
-                  <label class="control-label col-sm-4" for="param1">
+                <div className="form-group">
+                  <label className="control-label col-sm-4" for="param1">
                     No. Rujukan Makmal / <i>Lab Reference No.</i> :{' '}
                   </label>
-                  <div class="col-sm-8">
+                  <div className="col-sm-8">
                     <input
                       style={{ backgroundColor: '#eee' }}
                       type="email"
-                      class="form-control"
+                      className="form-control"
                       value="KPM/APRL26/MYEGS15"
                       readonly
                     />
                   </div>
                 </div>
-                <div class="form-group">
-                  <label class="control-label col-sm-4" for="param3">
+                <div className="form-group">
+                  <label className="control-label col-sm-4" for="param3">
                     Tarikh Makmal Terima Spesimen /{' '}
                     <i>Date Received Specimen in Lab (dd-mm-yyyy)</i> :{' '}
                   </label>
-                  <div class="col-sm-8">
+                  <div className="col-sm-8">
                     <input
                       style={{ backgroundColor: '#eee' }}
                       type="text"
-                      class="form-control"
+                      className="form-control"
                       value="26-04-2022"
                       readonly
                     />
                   </div>
                 </div>
-                <div class="form-group">
-                  <label class="control-label col-sm-4" for="param3">
+                <div className="form-group">
+                  <label className="control-label col-sm-4" for="param3">
                     Nama Ujian / <i>Test Name</i> :{' '}
                   </label>
-                  <div class="col-sm-8">
+                  <div className="col-sm-8">
                     <input
                       style={{ backgroundColor: '#eee' }}
                       type="text"
-                      class="form-control"
+                      className="form-control"
                       value="Rapid Antigen Test"
                       readonly
                     />
                   </div>
                 </div>
-                <div class="form-group">
-                  <label class="control-label col-sm-4" for="param3">
+                <div className="form-group">
+                  <label className="control-label col-sm-4" for="param3">
                     Keputusan Ujian / <i>Test Result</i> :{' '}
                   </label>
-                  <div class="col-sm-8">
+                  <div className="col-sm-8">
                     <input
                       style={{ backgroundColor: '#eee' }}
                       type="text"
-                      class="form-control"
+                      className="form-control"
                       value="Negative"
                       readonly
                     />
@@ -137,7 +137,7 @@ export default function Home() {
                   reference laboratory.
                 </i>
               </div>
-              <div class="col-sm-3">&nbsp;</div>
+              <div className="col-sm-3">&nbsp;</div>
             </div>
           </form>
         </div>
